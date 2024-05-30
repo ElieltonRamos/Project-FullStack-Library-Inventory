2c8eaf6ee4c7697c1ec65165a666d4e6a6df0899
Elielton <elieltonramos14@gmail.com>
2024-05-30 09:14:36 -0300

    feat(backend): :tada: created entities
    This commit adds the `Book` and `User` interfaces to the `backend-library-inventory` project. These interfaces define the structure and properties of a book and a user respectively. Additionally, the `NewBook` and `NewUser` types are defined as subsets of the original interfaces, omitting the `id` property. This will be useful for creating new book and user objects without specifying an ID.

---
+++ backend-library-inventory/src/interfaces/book.ts
@@ -0,0 +1,11 @@
+interface Book {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  status: string;
+}
+
+export type NewBook = Omit<Book, 'id'>;
+
+export default Book;
---
+++ backend-library-inventory/src/interfaces/user.ts
@@ -0,0 +1,10 @@
+interface User {
+  id: number;
+  displayName: string;
+  password: string;
+  image?: string;
+}
+
+export type NewUser = Omit<User, 'id'>;
+
+export default User;
