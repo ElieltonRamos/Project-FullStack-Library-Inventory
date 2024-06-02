c34e0984acfb117725ce1228aa593925bac21cff
Elielton <elieltonramos14@gmail.com>
2024-06-02 15:22:06 -0300

    feat(frontend): :art: Update Home page layout and navigation
    updates the layout and navigation of the Home page in the frontend library inventory project. It adds a navigation bar at the top of the page with a logo, a title, and a logout button. The main content section is also updated to include a placeholder section. This change improves the user experience and provides a consistent design across the application.

--- frontend-library-inventory/src/pages/Home.tsx
+++ frontend-library-inventory/src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  }
+
   return (
-    <main>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+    <main className="w-screen h-screen bg-brown_100 flex flex-col">
+
+      <nav className="h-14 w-full bg-white shadow-md flex justify-around">
+        <img src="https://cdn-icons-png.flaticon.com/512/2097/2097068.png" alt="logo" className="h-14 w-14" />
+        <h1 className="text-brown_300 font-bold text-md mt-3 ml-3">Library Inventory</h1>
+        <button
+          onClick={logout}
+          className="button bg-blue-600 text-white h-8 w-20 mt-3">
+          Logout
+        </button>
+      </nav>
+
+      <section className="flex justify-center items-center bg-gray-200 h-1 mt-10 w-3/4">
+        
+      </section>
     </main>
   )
 }
