56d25ea90563f38d4254e17bdbad904ed50ccbce
Elielton <elieltonramos14@gmail.com>
2024-06-03 10:46:30 -0300

    refactor(frontend):

--- frontend-library-inventory/src/App.tsx
+++ frontend-library-inventory/src/App.tsx
@@ -1,11 +1,18 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import BookPage from "./pages/BookPage"
 import NotFound from "./pages/NotFound"
 import Register from "./pages/Register"
+import { useEffect } from "react"
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) return navigate('/')
+  }, [navigate])
 
   return (
     <Routes>
